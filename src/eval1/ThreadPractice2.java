a490c1fdef72e680234b76a1901df16e8246db7a
Carlos <dag45684@educastur.es>
2023-10-08 11:20:24 +0200

    actividad 6 wack

--- src/eval1/ThreadPractice2.java
+++ src/eval1/ThreadPractice2.java
@@ -7,13 +7,19 @@ public class ThreadPractice2 {
 		Runnable task = new Task();
 		for(int i=0; i<5; i++) {
 			//Normal method
-//			new Thread(task, "Thread " + i).start();
+			Thread t = new Thread(task, "Thread " + i);
+			if(i%2==0) {
+				t.setPriority(i+i+1);
+			}else {
+				t.setPriority(i);
+			}
+			t.start();
 			//Lambda
-			new Thread(() -> {
-				try {
-					Thread.sleep(300);
-				} catch (InterruptedException e) {}
-			}, "lulz").start();
+//			new Thread(() -> {
+//				try {
+//					Thread.sleep(300);
+//				} catch (InterruptedException e) {}
+//			}, "lulz").start();
 			//Also possible with reference to function
 		}
 
